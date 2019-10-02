47d72359d206a4556eb970415d3488c492cfffe8
utec <cristopher.morales@utec.edu.pe>
2019-10-02 16:48:04 -0500

    uwu

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+typedef int uwu;
+int main() {
+    int *arr = new int [3]{10,20,30};
+    cout<< *(arr+2)<<endl;
+    cout << "uwu" <<endl;
+    return 0;
+}
